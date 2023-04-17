ded93b718f8ccce6c73bf8ca6c6eec5fc3fe06e0
David Levi <david_r_levi@outlook.com>
2023-04-17 17:01:18 +0100

    Adding comments to everything

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -12,10 +12,12 @@ public class MenuFunctions : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "Forest Speedway")
         {
+            //In-game, this is set to be the pause menu, so don't start with the pause menu open
             mainMenu.SetActive(false);
         }
         if (SceneManager.GetActiveScene().name == "Main Menu")
         {
+            //This is set to the main menu in the menu scene, so have the menu open on start-up
             mainMenu.SetActive(true);
         }
         instructions.SetActive(false);
@@ -29,21 +31,22 @@ public class MenuFunctions : MonoBehaviour
 
     public void GoToGame()
     {
-        Time.timeScale = 1;
-        if (SceneManager.GetActiveScene().name == "Forest Speedway")
+        //Unfreeze everything when returning from the pause menu, results screen
+        Time.timeScale = 1; //Or from the main menu after clicking 'back to menu' in the pause menu
+        if (SceneManager.GetActiveScene().name == "Forest Speedway") //In-game, this works a 'resume' function
         {
             mainMenu.SetActive(false);
             instructions.SetActive(false);
             Player.GetInstance().UnPause();
         }
-        else
+        else //In the main menu, this takes you to the game
         {
             SceneManager.LoadScene("Forest Speedway");
         }
 
-        if (Timer.GetInstance().timerOn == false)
+        if (Timer.GetInstance().timerOn == false) //The only situation where the timer is off is when the race is over
         {
-            SceneManager.LoadScene("Forest Speedway");
+            SceneManager.LoadScene("Forest Speedway"); //So this works as a restart function
         }
     }
 
@@ -51,25 +54,29 @@ public class MenuFunctions : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "Main Menu")
         {
+            //We're already in the menu, so just turn off the instructions. No need to reload the scene
             mainMenu.SetActive(true);
             instructions.SetActive(false);
         }
         else
         {
+            //In-game, this function is only tied to the 'back to menu' button
             SceneManager.LoadScene("Main Menu");
         }
     }
 
     public void ShowInstructions()
     {
+        //I was intially going to have the instructions available in the pause menu
+        //but switching them on and off didn't work right, so I just removed that button
         mainMenu.SetActive(false);
         instructions.SetActive(true);
     }
 
-    public void PauseGame()
+    public void PauseGame() //Called by the player with the 'P' key
     {
         Player.GetInstance().paused = true;
-        mainMenu.SetActive(true);
+        mainMenu.SetActive(true); //When the game's paused, show the menu
     }
 
     public void ExitGame()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,13 @@ public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
     public float turnspeed = 50.0f;
-    private int carrotSupply = 3;
+    private int carrotSupply = 3; //There's no getting these back, so use them wisely
 
-    public int lap;
-    bool messageShown;
+    private int lap; //The AI has their own lap variable though theirs is public
+    bool messageShown; //You don't need 'FINAL LAP!!!' flashing in your face all the time
 
     public GameObject carrot;
-    GameObject rival;
+    GameObject rival; //So that this script can tell the AI script what's up
 
     public TMP_Text lapText;
     public TMP_Text finalLapText;
@@ -19,7 +19,7 @@ public class Player : MonoBehaviour
     public TMP_Text carrotText;
     public GameObject resultsMenu;
 
-    float messageTimer;
+    float messageTimer; //How long stuff like 'FINAL LAP!' is on-screen
     public float timeLimit;
 
     private static Player instance;
@@ -61,20 +61,20 @@ public class Player : MonoBehaviour
         lapText.text = "Lap " + lap + "/" + Timer.GetInstance().lapLimit;
         carrotText.text = carrotSupply + " carrots left";
 
-        if (lap > Timer.GetInstance().lapLimit)
+        if (lap > Timer.GetInstance().lapLimit) //You've crossed the finish line. Huzzah!
         {
             victoryText.gameObject.SetActive(true);
-            paused = true;
-            Timer.GetInstance().timerOn = false;
-            resultsMenu.SetActive(true);
+            paused = true; //Pause the game so nothing WEIRD happens after the race ends
+            Timer.GetInstance().timerOn = false; //The race is over, you can stop the timer
+            resultsMenu.SetActive(true); //Rematch? Back to menu?
         }
-        else if (lap == Timer.GetInstance().lapLimit)
+        else if (lap == Timer.GetInstance().lapLimit) //FINAL LAP!!!
         {
-            if (!messageShown)
+            if (!messageShown) //When the message is shown, don't show again
             {
                 finalLapText.gameObject.SetActive(true);
-                messageTimer += Time.deltaTime;
-                if (messageTimer > timeLimit)
+                messageTimer += Time.deltaTime; //So that the message doesn't overstay it's welcome
+                if (messageTimer > timeLimit) //Go away when the time limit's up
                 {
                     finalLapText.gameObject.SetActive(false);
                     messageTimer = 0;
@@ -83,14 +83,15 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (Input.GetKey(KeyCode.P) && !paused)
+        if (Input.GetKey(KeyCode.P) && !paused) //You can't pause what is already paused
         {
-            MenuFunctions.GetInstance().PauseGame();
+            MenuFunctions.GetInstance().PauseGame(); //Gives the menu script it's cue
         }
 
         if (paused)
         {
-            Time.timeScale = 0;
+            Time.timeScale = 0; //Freeze everything
+            //Hide the in-game HUD elements while paused
             lapText.gameObject.SetActive(false);
             carrotText.gameObject.SetActive(false);
             rival.GetComponent<COMRacer>().staminaText.gameObject.SetActive(false);
@@ -99,10 +100,11 @@ public class Player : MonoBehaviour
         }
     }
 
-    public void UnPause()
+    public void UnPause() //Called by MenuFunctions upon clicking 'resume'
     {
-        paused = false;
+        paused = false; //No longer paused, so unfreeze everything
         Time.timeScale = 1;
+        //Show the HUD now that it's unpaused and hide the pause menu
         lapText.gameObject.SetActive(true);
         carrotText.gameObject.SetActive(true);
         rival.GetComponent<COMRacer>().staminaText.gameObject.SetActive(true);
